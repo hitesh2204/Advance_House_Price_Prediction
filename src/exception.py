86c85fd1dfeb03ec9af6a32682d41e07b7d6d13b
hitesh <hitesh.yerekar@gmail.com>
2026-07-27 18:43:19 +0530

    adding updated exception file.

---
+++ src/exception.py
@@ -0,0 +1,38 @@
+import sys
+from src.logger import logger
+
+
+def error_message_details(error,error_detail):
+
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    function_name = exc_tb.tb_frame.f_code.co_name
+    line_no = exc_tb.tb_lineno
+
+
+    error_message = f"""
+    Error occured in Project
+
+    File Name:{file_name}
+    Function Name:{function_name}
+    Line No:{line_no}
+    Error message:{str(error)}
+    """
+
+    return error_message
+
+
+class CustomException(Exception):
+
+    def __init__(self, error_message,error_detail):
+        super().__init__(error_message)
+
+        self.error_message = error_message_details(
+                error_message,
+                error_detail
+            )
+
+    def __str__(self):
+
+        return self.error_message
