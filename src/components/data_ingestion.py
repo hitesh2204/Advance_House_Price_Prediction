83d865886208f9bb7d9149ea54671f5a2198f0f6
hitesh <hitesh.yerekar@gmail.com>
2026-07-28 15:48:13 +0530

    data_ingestion and data_validation file added.

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+from src.logger import logger
+from src.exception import CustomException
+
+
+@dataclass
+class DataIngestionConfig:
+
+    train_data_path:str = os.path.join("artifacts","train.csv")
+
+    test_data_path:str = os.path.join("artifacts","test.csv")
+
+    raw_data_path:str = os.path.join("artifacts","raw.csv")
+
+
+class DataIngestion:
+
+    def __init__(self):
+
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            logger.info("Data Ingestion started.")
+
+            df = pd.read_csv("data/raw/train.csv")
+
+            logger.info("Dataset loaded successfully")
+
+            os.makedirs("artifacts",exist_ok=True)
+
+            logger.info("artifacts folder created successfully.")
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logger.info("Raw dataset saved into artifacts folder.")
+
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+            logger.info("Train-Test split completed successfully.")
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            logger.info("Train dataset saved to artifacts/train.csv")
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logger.info("Test dataset saved to artifacts/test.csv")
+
+            return (
+            self.ingestion_config.train_data_path,
+            self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e:
+
+            logger.error(CustomException(e,sys))
+
+            raise CustomException(e,sys)
+
+
+if __name__=="__main__":
+
+    data =  DataIngestion()
+    data.initiate_data_ingestion()
---
+++ src/components/data_validation.py
@@ -0,0 +1,96 @@
+import os
+import sys
+
+import pandas as pd
+
+from src.exception import CustomException
+from src.logger import logger
+from src.components.data_ingestion import DataIngestionConfig
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class DataValidationConfig:
+
+    data_validation_path:str = os.path.join("artifacts","data_validation_report.txt")
+
+class DataValidation:
+
+    def __init__(self):
+        self.validation_config = DataValidationConfig()
+
+    def initiate_data_validation(self,train_data,test_data):
+
+        try:
+            logger.info("Data Validation Started")
+
+            train_df = pd.read_csv(train_data)
+            test_df= pd.read_csv(test_data)
+
+            logger.info("Train and Test data loaded successfully.")
+
+            validation_status = True
+
+            report = []
+
+            if train_df.empty:
+                validation_status = False
+                report.append("Train dataset is empty")
+
+            if test_df.empty:
+                validation_status = False
+                report.append("Test dataset is empty")
+
+            ## Checking dataset shape
+            report.append(f"Train shape:{train_df.shape}")
+            report.append(f"Test shape:{test_df.shape}")
+
+            ## checking missing values in dataset.
+            train_missing_data = train_df.isnull().sum()
+            test_missing_data = test_df.isnull().sum()
+
+            report.append(f"Train missing values:{train_missing_data}")
+            report.append(f"Test missing values:{test_missing_data}")
+
+            ## checking the duplicates data.
+            train_duplicated_data = train_df.duplicated().sum()
+            test_duplicated_data = test_df.duplicated().sum()
+
+            report.append(f"Train duplicated data:{train_duplicated_data}")
+            report.append(f"Test duplicated data:{test_duplicated_data}")
+
+            if validation_status:
+                report.append("Data validation passed")
+                logger.info("Data validation successfully")
+
+            else:
+                report.append("Data validation failed")
+                logger.info("Data validation failed")
+
+            ## save validation report.
+
+            with open(self.validation_config.data_validation_path,'w') as file:
+                for line in report:
+                    file.write(line + "\n")
+            logger.info("Validation report generated successfully.")
+
+            return validation_status
+
+        except Exception as e:
+
+            logger.error(CustomException(e,sys))
+
+            raise CustomException(e,sys)
+
+
+if __name__ =="__main__":
+
+    data = DataValidation()
+    ingestion_config = DataIngestionConfig()
+
+    train_data = ingestion_config.train_data_path
+    test_data = ingestion_config.test_data_path
+
+    data.initiate_data_validation(train_data,test_data)
